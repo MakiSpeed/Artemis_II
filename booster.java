f991d42af8d2c24835c65c1c1599a920229ed5a6
Marie-Catalina Grenot <mariecatalina.grenot@ynov.com>
2026-04-27 16:18:23 +0200

    feat(booster): add class booster

---
+++ booster.java
@@ -0,0 +1,30 @@
+public class booster {
+    private String _nom;
+
+    public String nom ; {
+        get { return _nom; }
+        set { nom = value; } ;
+    }
+
+    private int pousseekN;
+
+    public int pousseekN ; {
+        get { return pousseekN; } ;
+        set { pousseekN = value; } ;
+    }
+
+    private double masse;
+
+    public double masse ; {
+        get { return masse; } ;
+        set { masse = value; } ;
+    }
+
+    private double prix;
+
+    public double prix ; {
+        get { return prix; } ;
+        set { prix = value; } ;
+    }
+
+}
