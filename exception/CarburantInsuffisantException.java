262a007ad5e0b362d0173fa7b7a285f03ec78071
Marie-Catalina Grenot <mariecatalina.grenot@ynov.com>
2026-05-06 09:08:19 +0200

    feat(exception_carburant): add Carburant Insuffisant Exception

--- exception/CarburantInsuffisantException.java
+++ exception/CarburantInsuffisantException.java
@@ -1,3 +1,14 @@
-public class CarburantInsuffisantException {
-    
+public class CarburantInsuffisantException extends Exception {
+
+    private final double carburantNecessaire;
+    private final double carburantMax;
+
+    public CarburantInsuffisantException(double carburantNecessaire, double carburantMax) {
+        super("Carburant insuffisant : " + carburantNecessaire + "t nécessaires, " + carburantMax + "t disponibles");
+        this.carburantNecessaire = carburantNecessaire;
+        this.carburantMax = carburantMax;
+    }
+
+    public double getCarburantNecessaire() { return carburantNecessaire; }
+    public double getCarburantMax() { return carburantMax; }
 }
